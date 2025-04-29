bcaa6a86812b67779ec109d70a7b7cf702b6baa8
gpelincel <gabrielpelincel@gmail.com>
2025-04-29 15:34:15 -0300

    New Feature: Projetos controller

--- app/Http/Controllers/ProjetoController.php
+++ app/Http/Controllers/ProjetoController.php
@@ -12,4 +12,49 @@ class ProjetoController extends Controller
 
         return view('projetos', compact('projetos'));
     }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) {
+        $projeto = Projeto::create($request->all());
+
+        return redirect()->back()->with('status', 'success')->with('message', 'Projeto cadastrado com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id) {
+        $projeto = Projeto::findOrFail($id);
+
+        return view('projeto-info', compact('projeto'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(projeto $projeto) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id) {
+        $projeto = Projeto::findOrFail($id);
+        $projeto->update($request->all());
+
+        return redirect()->back()->with('status', 'success')->with('message', 'Projeto atualizado com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id) {
+        $projeto = Projeto::findOrFail($id);
+        $projeto->delete();
+
+        return redirect()->back()->with('status', 'success')->with('message', 'projeto deletado com sucesso!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,27 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/sobre-nos', function () {
+    return view('sobre-nos');
+});
+
+Route::get('/voluntarios', function () {
+    return view('voluntarios');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/fale-conosco', function () {
+    return view('fale-conosco');
+});
+
+Route::get('/doar', function () {
+    return view('doar');
+});
+
 Route::get('/projetos', [ProjetoController::class, 'index']);
+Route::get('/projetos/{id}', [ProjetoController::class, 'show']);
+Route::post('/projetos', [ProjetoController::class, 'save']);
+Route::put('/projetos', [ProjetoController::class, 'update']);
