124d04b4a8eb73ac19ff423349280e5b9760b064
gpelincel <gabrielpelincel@gmail.com>
2025-04-29 14:21:22 -0300

    New Feature: Home page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="flex justify-between p-4 bg-ocean-900 text-white">
+    <div class="logo">
+        <h1>{Código_Aberto}</h1>
+    </div>
+    <ul class="flex gap-8">
+        <li>Sobre Nós</li>
+        <li>Projetos</li>
+        <li>Voluntários</li>
+        <li>FAQ</li>
+        <li>Fale Conosco</li>
+        <li>Doar</li>
+    </ul>
+    <ul class="flex gap-3">
+        <li>
+            <a class="bg-ocean-700 text-white py-2 px-4 rounded" href="">Inscreva-se</a>
+        </li>
+        <li>
+            <a class="py-2 px-4" href="">Login</a>
+        </li>
+    </ul>
+</nav>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<x-layouts.app>
+    <section class="flex flex-col justify-center bg-hero min-h-[92vh]">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+            <h1 class="mb-4 text-4xl font-extrabold text-ocean-100">
+                Conectando Desenvolvedores Iniciantes a Oportunidades Reais</h1>
+            <p class="mb-8 text-lg font-normal lg:text-xl sm:px-16 lg:px-48 text-gray-300">Ganhe experiência prática,
+                construa seu portfólio e aumente suas chances no mercado de trabalho.</p>
+            <div class="flex gap-6 items-center justify-center">
+                <a href="#"
+                    class="bg-ocean-800 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg">
+                    Comece Agora
+                    <i class="ml-2 fa-solid fa-arrow-right"></i>
+                </a>
+                <a href="#" class="font-medium text-ocean-100">
+                    Saiba Mais
+                </a>
+            </div>
+        </div>
+    </section>
+
+</x-layouts.app>
