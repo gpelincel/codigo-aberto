18ffeeeb0817b1fb6d463a2b2840e027006d0abd
gpelincel <gabrielpelincel@gmail.com>
2025-05-02 14:42:23 -0300

    Bug Fix: Session após cadastrar

--- app/Http/Controllers/ONGController.php
+++ app/Http/Controllers/ONGController.php
@@ -6,14 +6,14 @@ use App\Models\ONG;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class ONGController extends Controller
-{
+class ONGController extends Controller {
     public function store(Request $request) {
         $request['senha'] = Hash::make($request['senha']);
         $ong = ONG::create($request->all());
         session(['user_id' => $ong->id]);
+        session(['username' => $ong->nome]);
 
-        return redirect('/ong/'.$ong->id)->with('status', 'success')->with('message', 'ONG cadastrada com sucesso!');
+        return redirect('/ong/' . $ong->id)->with('status', 'success')->with('message', 'ONG cadastrada com sucesso!');
     }
 
     public function show(string $id) {
@@ -28,9 +28,7 @@ class ONGController extends Controller
         if ($usuario && Hash::check($request['senha'], $usuario->senha)) {
             session(['user_id' => $usuario->id]);
             session(['username' => $usuario->nome]);
-            return redirect('/ong/'.$usuario->id)->with('status', 'success')->with('message', 'ONG cadastrada com sucesso!');
+            return redirect('/ong/' . $usuario->id)->with('status', 'success')->with('message', 'ONG cadastrada com sucesso!');
         }
     }
-
-    
 }
