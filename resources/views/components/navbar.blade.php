5bffafc4f4cda437a99d3cab3a26e8f54381fca3
gpelincel <gabrielpelincel@gmail.com>
2025-05-05 14:27:14 -0300

    Bug Fix: Navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,6 +12,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
         </button>
+        
 
         <!-- Menu completo em telas grandes -->
         <ul class="hidden lg:flex gap-8 items-center">
